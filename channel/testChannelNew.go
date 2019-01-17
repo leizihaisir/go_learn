6059910aec983d74669935fb552ad95044e963bb
zihailei <zihailei@creditease.cn>
2019-01-17 15:19:46 +0800

    struct method学习

---
+++ channel/testChannelNew.go
@@ -0,0 +1,14 @@
+package channel
+
+import "fmt"
+
+func main() {
+	var ch chan int = make(chan int, 4)
+	for i := 0; i < cap(ch); i++ {
+		ch <- i // 写通道
+	}
+	for len(ch) > 0 {
+		var value int = <-ch // 读通道
+		fmt.Println(value)
+	}
+}
